ab2c7abaa8613890cd263814b3ade4a06719480b
AlexandreJusten <58986915+AlexandreJusten@users.noreply.github.com>
2024-01-04 17:02:15 -0300

    init

---
+++ src/test/java/com/api/iphatcom/IphatcomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.iphatcom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IphatcomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
